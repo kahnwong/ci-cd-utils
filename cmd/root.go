fcafc9d7d9e5a8fe4cf1bef5ed5caf45a6d93274
Karn Wong <karn@karnwong.me>
2024-10-25 21:23:14 +0700

    ci: add goreleaser

--- cmd/root.go
+++ cmd/root.go
@@ -6,9 +6,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	version = "dev"
+)
+
 var rootCmd = &cobra.Command{
-	Use:   "ci-cd-utils",
-	Short: "Various utils for CI/CD",
+	Use:     "ci-cd-utils",
+	Version: version,
+	Short:   "Various utils for CI/CD",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
